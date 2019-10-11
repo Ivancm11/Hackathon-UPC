6f25533bed9a26704ccb559edfbe1931c6005e55
Ivan <ivan11cm@gmail.com>
2019-10-11 22:43:12 +0200

    first commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'leagueoflegends';
+	
+	$connect = new mysqli($hostname,$username,$password,$database);
+?>
