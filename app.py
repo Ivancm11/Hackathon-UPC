80d955125c9340764251c5e3f08a3c6154b754ae
victorgarciareolid <victorgarciavilu@gmail.com>
2019-10-12 00:31:59 +0200

    Uploading

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import send_from_directory, redirect
+from flask import send_from_directory, redirect, request
+import os
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+path_to_upload = "/home/victor/Hackathon-UPC/upload" # change in docker
 
 @app.route("/")
 def main():
@@ -10,6 +12,22 @@ def main():
 @app.route("/hola", methods=["GET"])
 def holita():
     return "holita"
+
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print("Not a file")
+            return "NO"
+        file = request.files['file']
+        if file.filename=='':
+            return "NO"
+        print(file)
+        if file:
+            print("Saving " + os.path.join(path_to_upload, file.filename))
+            file.save(os.path.join(path_to_upload, file.filename))
+        return "SI"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
