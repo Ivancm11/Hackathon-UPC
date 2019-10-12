207054488b636b29b77d626946026384d5b281da
Ivan <ivan11cm@gmail.com>
2019-10-12 10:55:45 +0200

    uploades webpage to static folder

---
+++ WEBPAGE/like_button.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+const domContainer2 = document.querySelector('#like_button_container2');
+ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(LikeButton), domContainer2);
