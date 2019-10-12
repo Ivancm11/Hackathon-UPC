f170bca1d9ac46113963a1f6351a8ee6a7358e8e
victorgarciareolid <victorgarciavilu@gmail.com>
2019-10-12 20:57:01 +0200

    compute co2 api added

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ cnx = mysql.connector.connect(host='remotemysql.com',
                             port=3306)
 cursor = cnx.cursor()
 
+def computeCo2(calories, carbohydrates, protein, fat, saturatedFats, salt, co2):
+    return 0.0
+
 @app.route("/")
 def main():
     return redirect("index.html")
@@ -49,21 +52,38 @@ def search():
     print(name)
    
     # return json with data
-    query = "SELECT * FROM foods WHERE name LIKE '%{}%'".format(name)
+    query = "SELECT * FROM foods2 WHERE name LIKE '%{}%'".format(name)
     cursor.execute(query)
     
-    for (name, type_, energy, grasas, sugar, h2o, co2) in cursor:
-        print((name, type_, energy, grasas, sugar, h2o, co2))
+    for (name, calories, carbohydrates, protein, fat, saturatedFats, salt, co2) in cursor:
 
         result_json["data"].append({
             "name":name,
-            "type":type_,
-            "energy":energy,
-            "grasas": grasas,
-            "sugar":sugar,
-            "H2O":h2o,
-            "CO2":co2,
+            "calories":calories,
+            "carbohydrates":carbohydrates,
+            "protein": protein,
+            "fat":fat,
+            "saturatedFats":saturatedFats,
+            "salt":salt,
+            "co2":co2,
         })
+
+    return jsonify(result_json)
+
+
+@app.route("/computeCo2", methods=['GET'])
+def compute_co2():
+    result_json = {}
+    
+    calories = request.args.get('name')
+    carbohydrates = request.args.get('carbohydrates')
+    protein = request.args.get('protein')
+    fat = request.args.get('fat')
+    saturatedFats = request.args.get('saturatedFats')
+    salt = request.args.get('salt')
+    co2 = request.args.get('co2')
+
+    result_json = {'result':computeCo2(calories, carbohydrates, protein, fat, saturatedFats, salt, co2)}
 
     return jsonify(result_json)
 
